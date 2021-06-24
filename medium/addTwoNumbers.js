383723c09af3add55f120e54634bb25737ced5cd
vinccodes <vinccodes@gmail.com>
2021-06-23 23:56:35 -0400

    First commit

---
+++ medium/addTwoNumbers.js
@@ -0,0 +1,18 @@
+// Examples
+
+// L1 = [1, 2, 3]
+// L2 = [4, 5, 6]
+
+// 975 = 321 + 654
+
+function addTwoNumber(l1, l2) {
+    // traverse the linked-list backwards
+
+        // grab the value and append the empty string
+
+    // convert string to integer
+
+    // sum two numbers
+}
+
+console.log(addTwoNumbers([1,2,3], [4,5,6]), 975); 
