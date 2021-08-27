cec39102cdbddb3f57cd9f180117767f351581df
vinccodes <vinccodes@gmail.com>
2021-08-27 09:41:03 -0400

    Finished majority-element

--- easy/169-majority-element.js
+++ easy/169-majority-element.js
@@ -16,6 +16,35 @@ Output: 2
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function(nums) {
+ var majorityElement = function(nums) {
+    const hashMap = {}
+    let maxCount = 0;
+    let majority = 0
     
+    // case for only 1 element
+    if (nums.length == 1) {
+        return nums[0]
+    }
+    
+    // iterate through nums 
+    for (let i = 0; i < nums.length; i++) {
+        // if the number doesn't exist add it to hashmap
+        if (hashMap[nums[i].toString()]) {
+            hashMap[nums[i].toString()] += 1;
+            
+            if (hashMap[nums[i].toString()] > maxCount) {
+                maxCount = hashMap[nums[i].toString()]
+                majority = nums[i]
+            }
+            // check if the count of the element appears more than n / 2 times
+            if (hashMap[nums[i].toString()] > nums.length / 2) {
+                return nums[i]
+            }
+        }
+        else {
+            hashMap[nums[i].toString()] = 1
+            
+        }
+    }
+    return majority
 };
